3887e9a22c8e4b714975130d3dfe196a472b2e0f
stpad <sutanpangeranaryadewa@gmail.com>
2026-04-13 23:18:53 +0700

    i added "Operator-Aritmatika" folder on "Jam-2" folder

---
+++ Js-Dasar/Jam-2/Operator-Aritmatika/index.js
@@ -0,0 +1,45 @@
+let angka1 = 9 - 7;
+let angka2 = 5 + 3;
+
+document.writeln(angka1);
+document.writeln("<br>");
+document.writeln(angka2);
+
+let angka3 = 10;
+let angka4 = 2;
+
+document.writeln("<br>");   
+document.writeln("\"" + angka3 * angka4 + "\"");
+
+let hasil = angka3 / 4;
+document.writeln("<br>");   
+document.writeln("hasil 10 bagi 4 adalah : " + hasil);    
+document.writeln("<br>");
+
+let angka5 = 67;
+angka5 /= hasil;
+
+document.writeln(angka5);
+document.writeln("<br>");
+
+hasil = angka5;
+hasil += angka2;
+
+document.writeln(hasil);
+document.writeln("<br>");
+
+let angka6 = 6 % hasil;
+
+document.writeln(angka6);
+document.writeln("<br>");
+
+hasil = angka6;
+let angka7 = hasil ** 2;
+
+document.writeln(angka7);
+document.writeln("<br>");
+
+hasil = ++angka7;
+document.writeln(hasil);
+
+
