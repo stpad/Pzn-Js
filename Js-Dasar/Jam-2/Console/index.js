409cff5a6a4a05a281db490064e046c3f0a5c43d
stpad <sutanpangeranaryadewa@gmail.com>
2026-04-17 00:08:24 +0700

    minor modified on index.js file at "Console" folders, and added "String-Template" folder

---
+++ Js-Dasar/Jam-2/Console/index.js
@@ -0,0 +1,13 @@
+console.log("Sutan \t Gosling");
+console.warn("\"test\"");
+console.error("Saya \n Sutan Gosling");
+console.debug("test-debug");
+
+nilaiMtk = 100;
+nilaiKalkulus = 100;
+
+lulusMTK = nilaiMtk >= 95;
+lulusKalkulus = nilaiKalkulus >= 95;
+
+lulusKelas = lulusMTK && lulusKalkulus;
+console.warn(lulusKelas);
