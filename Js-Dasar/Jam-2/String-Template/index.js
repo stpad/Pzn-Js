a98d048188b97fc89b7a10e331c4f5661c94afca
stpad <sutanpangeranaryadewa@gmail.com>
2026-04-20 22:31:32 +0700

    minor modified on "string template" folder

---
+++ Js-Dasar/Jam-2/String-Template/index.js
@@ -0,0 +1,11 @@
+let $namaDepan = 'Sutan';
+let $namaBelakang = 'Gosling';
+
+document.writeln(`${$namaDepan} ${$namaBelakang}`); 
+document.writeln(`</br>`);
+document.writeln(`${$namaDepan} ${$namaBelakang} adalah seorang aktor`);
+
+let angka1 = 10;
+let angka2 = 20;
+document.writeln(`</br>`);
+document.writeln(`${angka1 * angka2}`);
