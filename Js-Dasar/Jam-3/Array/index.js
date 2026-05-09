c4af2d51c28a3f4180040c80dae0117184c85cf3
stpad <sutanpangeranaryadewa@gmail.com>
2026-05-09 17:07:25 +0700

    minor modified on array folder

--- Js-Dasar/Jam-3/Array/index.js
+++ Js-Dasar/Jam-3/Array/index.js
@@ -7,7 +7,7 @@ console.table(data); // Output: Tabel dengan indeks dan nilai array
 
 
 let angka = [];
-angka.push(67);
+angka.push(67 + 90);
 angka.push(89);
 angka.push(45);
 document.writeln(angka.length);    
@@ -21,3 +21,10 @@ document.writeln("<br>");
 
 delete angka[1];
 document.writeln(angka[1]); // Output: undefined
+document.writeln("<br>");
+document.writeln(angka[0]); 
+document.writeln("<br>");
+
+let array1 = ["A", "B", "C"];
+let array2 = [array1, "D", "E", "F"];
+console.table(array2); // Output: Tabel dengan array1 sebagai elemen pertama dan "D", "E", "F" sebagai elemen berikutnya
