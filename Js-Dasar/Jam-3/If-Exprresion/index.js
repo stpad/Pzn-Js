4aa97db42352e063abc05822406e8f9182d8392e
stpad <sutanpangeranaryadewa@gmail.com>
2026-05-15 23:11:18 +0700

    adding "pop-up" folder

---
+++ Js-Dasar/Jam-3/If-Exprresion/index.js
@@ -0,0 +1,10 @@
+let nilaiUjian = 20;
+if (nilaiUjian === 100) {
+    document.writeln(`Nilai Antum ${nilaiUjian}, Jadi Dosen Lu`);
+} else if (nilaiUjian >= 90) {
+    document.writeln(`Nilai Antum ${nilaiUjian}, Nilai A`);
+} else if (nilaiUjian >= 80) {
+    document.writeln(`Nilai Antum ${nilaiUjian}, Nilai B`);
+} else {
+    document.writeln(`Nilai Antum ${nilaiUjian}, Belajar lagi Nigga`);
+}
