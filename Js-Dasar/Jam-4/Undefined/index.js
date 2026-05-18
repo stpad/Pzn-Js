3217ef3b31bdc49c19a0fd77a94a76714f4272eb
stpad <sutanpangeranaryadewa@gmail.com>
2026-05-18 14:59:41 +0700

    adding "Undefined" folder

---
+++ Js-Dasar/Jam-4/Undefined/index.js
@@ -0,0 +1,9 @@
+let name;
+console.log(name); // Output: undefined
+
+let nama = ["Sutan Goslong", "Ryan Gosling", "Brad Pitt", "Sutan Gosling"];
+if(nama[3]) {
+    console.log(`Halo ${nama[3]}`);
+} else {
+    console.log("Maaf, nama tidak ditemukan.");
+}
