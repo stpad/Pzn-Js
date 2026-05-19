7a1c00ca7bf19d3154108e1b26b867210e132876
stpad <sutanpangeranaryadewa@gmail.com>
2026-05-19 23:23:57 +0700

    minor modiefied on "swith expression" folder

--- Js-Dasar/Jam-4/Switch-Expression/index.js
+++ Js-Dasar/Jam-4/Switch-Expression/index.js
@@ -1,6 +1,14 @@
-let nilai = 98;
-switch (nilai){
+let nilai = prompt('Masukkin nilai tong (1-100)');
+switch (true) {
     case nilai >= 90:
-        document.writeln(`Mamah kamu bangga karena dapet nilai ${nilai}`);
+        alert(`Mamah kamu bangga karena dapet nilai ${nilai}`);
         break;
+    case nilai >= 70:
+        alert(`Mamah kamu lumayan bangga karena dapet nilai ${nilai}`);
+        break;
+    case nilai >= 50:
+        alert(`dongok banget luwh dapet nilai ${nilai}`);
+        break;
+    default:
+        alert(`Drop Out lu malu2in mamah kamu karena dapet nilai ${nilai}`);
 }
