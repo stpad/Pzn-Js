c80d0e94ea319456ddc74bfd2634b7431eeffc93
stpad <sutanpangeranaryadewa@gmail.com>
2026-05-26 15:23:33 +0700

    adding "with-statement" folder

---
+++ Js-Dasar/Jam-5/With-Statement/index.js
@@ -0,0 +1,9 @@
+let orang = {
+    nama: "Sutan Gosling",
+    umur: 30,
+    pekerjaan: "Pengganguran"
+}
+
+with (orang){
+    document.writeln(`Nama: ${nama} <br> Umur: ${umur} <br> Pekerjaan: ${pekerjaan}`);
+}
