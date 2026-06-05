a8b35afa8a145c96d569f825edcd73acfc71a176
stpad <sutanpangeranaryadewa@gmail.com>
2026-06-05 23:26:11 +0700

    adding "function-generator" folder

--- Js-Dasar/Jam-6/Latihan-6/No-15/index.js
+++ Js-Dasar/Jam-6/Latihan-6/No-15/index.js
@@ -1,3 +1,16 @@
-function buatPesanan(nama = "Guest", alamat = "Tidak Tersedia", ...produk){
-    
+let nama = prompt(`Masukkan nama`);
+let alamat = prompt(`Masukkan alamat`);
+let produk = [];
+for (let i = 0; i < produk.length; i++){
+    let item = prompt(`Masukkan produk ke-${i + 1}`);
+    produk.push(item);
 }
+function buatPesanan(nama = "Guest", alamat = "Tidak Tersedia", ...produk){
+
+}
+
+
+let hasilPesanan = buatPesanan();
+document.write(`Nama: ${hasilPesanan.nama} <br>`);
+document.write(`Alamat: ${hasilPesanan.alamat} <br>`);
+document.write(`Produk: ${hasilPesanan.produk}`);
