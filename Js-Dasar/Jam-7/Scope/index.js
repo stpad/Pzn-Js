d8cc80a2489118b70e45c8e8454a1bb050837f2b
stpad <sutanpangeranaryadewa@gmail.com>
2026-06-22 23:53:27 +0700

    adding "latihan-7/scope" folder

--- Js-Dasar/Jam-7/Scope/index.js
+++ Js-Dasar/Jam-7/Scope/index.js
@@ -1,10 +1,20 @@
-function a (){
-let a = 1;
-console.log(a);
+function a() {
+    let a = 1;
+    // console.log(a);
+
+    function aDalam() {
+        let c = 3;
+        let jumlah = a + c;
+        console.log(jumlah);
+    }
+    
+    aDalam();
+    console.log(jumlah); // error karena jumlah hanya bisa diakses di dalam function aDalam
+
 }
 
-function b (){
-let b = 2;
+function b() {
+    let b = 2;
 }
 
 a();
